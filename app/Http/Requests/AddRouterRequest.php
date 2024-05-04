1fd351178e932a1e76e9d7155838b92436a363b8
Tri <triadilaksamana@gmail.com>
2024-05-04 11:52:51 +0700

    Update package name to "venda-net"

--- app/Http/Requests/AddRouterRequest.php
+++ app/Http/Requests/AddRouterRequest.php
@@ -24,7 +24,10 @@ class AddRouterRequest extends FormRequest
         return [
             'host' => ['required', 'string'],
             'username' => ['required', 'string'],
-            'password' => ['required', 'string'],
+            'password' => ['string'],
+            'auto_isolir' => ['required', 'boolean'],
+            'isolir_action' => ['required', 'string', 'in:change_profile,disabled_secret'],
+            'isolir_profile' => ['required_if:isolir_action,change_profile', 'string'],
         ];
     }
 }
--- resources/views/content/router/edit.blade.php
+++ resources/views/content/router/edit.blade.php
@@ -3,58 +3,68 @@
 @section('title', 'Add Router')
 
 @section('content')
-<h4 class="py-3 mb-4"><span class="text-muted fw-light">Router/</span> Edit #1</h4>
+<h4 class="py-3 mb-4"><span class="text-muted fw-light">Router/</span> Create</h4>
 
 <!-- Basic Layout -->
-<div class="row">
-    <div class="col-xl">
-        <div class="card mb-4">
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="mb-0">Edit Router</h5> <small class="text-muted float-end">Default label</small>
-            </div>
-            <div class="card-body">
-                <form>
+<form action="{{ route("router-settings.store") }}">
+    <div class="row">
+        <div class="col-xl">
+            <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5 class="mb-0">Tambah Router</h5> <small class="text-muted float-end">Default label</small>
+                </div>
+                <div class="card-body">
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-fullname">Host</label>
-                        <input type="text" class="form-control" id="basic-default-fullname" placeholder="Remote URL" />
+                        <label class="form-label" for="host">Host</label>
+                        <input type="text" class="form-control" name="host" id="host" placeholder="Remote URL" />
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-company">Username</label>
-                        <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc." />
+                        <label class="form-label" for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="password">Password</label>
-                        <input type="password" id="password" class="form-control" placeholder="Input your password" />
+                        <input type="password" id="password" name="password" class="form-control"
+                            placeholder="Input your password" />
                     </div>
-                    <button type="submit" class="btn btn-primary">Test Connection</button>
-                </form>
+                    <button class="btn btn-primary">Test Connection</button>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xl">
-        <div class="card mb-4">
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="mb-0">Setting Isolir</h5> <small class="text-muted float-end">Default label</small>
-            </div>
-            <div class="card-body">
-                <form>
+        <div class="col-xl">
+            <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5 class="mb-0">Setting Isolir</h5>
+                </div>
+                <div class="card-body">
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-fullname">Auto Isolir ?</label>
-                        <input type="text" class="form-control" id="basic-default-fullname" placeholder="Remote URL" />
+                        <label class="form-label" for="auto_isolir">Auto Isolir ?</label>
+                        <select class="form-select" id="auto_isolir" name="auto_isolir">
+                            <option value="1">Ya</option>
+                            <option value="0">Tidak</option>
+                        </select>
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-company">Action Isolir</label>
-                        <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc." />
+                        <label class="form-label" for="isolir_action">Action Isolir</label>
+                        <select class="form-select" id="isolir_action" name="isolir_action">
+                            <option value="change_profile">UBAH SECRET PROFILE</option>
+                            <option value="disabled_secret">DISABLE SECRET</option>
+                        </select>
                     </div>
                     <div class="mb-3">
-                        <label class="form-label" for="password">Pilih Profile Isolir</label>
-                        <input type="password" id="password" class="form-control" placeholder="Input your password" />
+                        <label class="form-label" for="isolir_profile">Pilih Profile Isolir</label>
+                        <select class="form-select" id="isolir_profile" name="isolir_profile">
+                            {{-- @TODO: dinamik --}}
+                            <option value="change_profile">1 MB</option>
+                            <option value="disabled_secret">DISABLE</option>
+                        </select>
                     </div>
                     <button type="submit" class="btn btn-primary">Simpan</button>
-                </form>
+                    <button class="btn btn-secondary">Batal</button>
+                </div>
             </div>
         </div>
     </div>
+</form>
 
-</div>
 @endsection
